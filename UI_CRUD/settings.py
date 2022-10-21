b40438e77432b224fa3527309dba20ab93dd143b
kanilkumarreddy <116055966+kanilkumarreddy@users.noreply.github.com>
2022-10-21 14:48:59 +0530

    Update settings.py

--- UI_CRUD/settings.py
+++ UI_CRUD/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME':'Kanini',
         'USER':'postgres',
         'PASSWORD':'Anil@1234',
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT':'5432'
     }
 }
