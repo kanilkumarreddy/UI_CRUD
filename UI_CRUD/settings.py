1c3a72059e8280b1dc4394b336b2cd63fb1a4ae9
kanilkumarreddy <116055966+kanilkumarreddy@users.noreply.github.com>
2022-10-31 12:50:13 +0530

    Update settings.py

--- UI_CRUD/settings.py
+++ UI_CRUD/settings.py
@@ -51,11 +51,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8081',
-)
-
 ROOT_URLCONF = 'UI_CRUD.urls'
 
 TEMPLATES = [
